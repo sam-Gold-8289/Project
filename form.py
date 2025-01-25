7995a447d7f8ecb64f72aba08912b4bdb90f1ce3
sam-Gold-8289 <sdktrade8289@gmail.com>
2025-01-26 01:42:52 +0500

    changes to form.py

--- form.py
+++ form.py
@@ -10,9 +10,14 @@ second_date = datetime.datetime.now()
 age = second_date.year - first_date.year
 
 if age in range (1, 13):
-    print(name, "малолетка ты еще по таким сайтам шарить")
+    print(name.title(), ", малолетка ты еще по таким сайтам шарить.")
 elif age in range (14, 17):
-    print(name, "можешь конечно попробывать, но думaю не надо")
+    print(name.title(), ", можешь конечно попробывать, но думaю не надо.")
 elif age in range (18, 100):
-    print(name, "Добро пожаловать скуф")
+    print(name.title(), "- Добро пожаловать!")
+    x=open('user_1.txt', 'a+')
+    x.write(name.title() + "- Больше 18 лет"+'\n')
+    x.close()
+
+
 
